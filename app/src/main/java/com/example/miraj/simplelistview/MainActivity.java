8f23a4293c2dbab130bf2bbe28ffd4d78cfc22f2
Mirajul Hoq <42499496+mirajulhoque@users.noreply.github.com>
2018-10-04 12:31:07 +0600

    Initial commit

---
+++ app/src/main/java/com/example/miraj/simplelistview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.miraj.simplelistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    private String[] countryNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listview);
+        countryNames=getResources().getStringArray(R.array.country_name);
+
+        ArrayAdapter adapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,countryNames);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Toast.makeText(MainActivity.this, countryNames[position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
